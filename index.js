7ea0fa257bd6389a0b32c59e99d7d801e69aa1d9
Kevina <kevinanakayiza@gmail.com>
2025-07-18 14:24:19 +0300

    added Js to display the number of items in the todo

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ save.addEventListener("click", function(){
     const taskText = input.value.trim();
     if (taskText !==""){
       
-       
-      
-
-        const taskItem = document.createElement("li");
+      const taskItem = document.createElement("li");
        taskItem.textContent = taskText;
     
        input.value = "";
        taskItem.style.listStyle = "none";
        taskItem.style.padding = "8px";
-      
+
+    
+
+
+
+
        const radioButton = document.createElement("input");
        radioButton.type = "radio";
        radioButton.style.border ="1px solid blue";
@@ -25,22 +27,43 @@ save.addEventListener("click", function(){
       container.appendChild(taskItem)
       tasklist.appendChild(container)
       container.style.display = "flex"
-    //   container.style.flexDirection = "row-reverse"
       container.style.borderBottom = "1px solid grey";
     
       radioButton.addEventListener("click", function(){
         taskItem.style.textDecoration = "line-through"
+        taskItem.style.color = "grey"
          
        })
     }
 
+  //  if (nav.length > 0){
+  //   for (let K = 0; K<=0; K++);{
+  //     let itemNumber = document.createElement("p");
+  //     itemNumber.id ="itemsleft";
+  //     itemNumber.textContent = "0 items left";
+  //   nav[0].appendChild(itemNumber);
+  //   function updateItemsLeft(count){
+  //     document.getElementById("itemsleft").textContent = `$ {count} items left`;
+  //   }
+  //   }
     
-});
+  //  }
+   
+    const nav = document.getElementsByClassName("nav");
+    if (nav.length > 0){
+     
+      for (let K = 0; K <=0; K++){
+        const itemNumber = document.createElement("p");
+        itemNumber.textContent = `${K} Items left`;
+        nav[0].appendChild(itemNumber)
+      }
+    }
+  });
+  
 
-
+ 
+ 
 
           
        
-    //    tasklist.style.display = "flex";
-    //    tasklist.style.flexDirection = "row";
-    //    tasklist.style.gap = "3px";
+    
